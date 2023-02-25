32377899cae2763853472fdb7246bc2431ec4aae
= <=>
2023-02-25 11:13:38 +0500

    Initial Commit

---
+++ T-Rex Game/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace T_Rex_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.trex = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.textScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trex
+            // 
+            this.trex.Image = global::T_Rex_Game.Properties.Resources.running;
+            this.trex.Location = new System.Drawing.Point(110, 363);
+            this.trex.Name = "trex";
+            this.trex.Size = new System.Drawing.Size(40, 43);
+            this.trex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.trex.TabIndex = 0;
+            this.trex.TabStop = false;
+            this.trex.Tag = "trex";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::T_Rex_Game.Properties.Resources.obstacle_1;
+            this.pictureBox2.Location = new System.Drawing.Point(640, 348);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(23, 46);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "obstacle";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::T_Rex_Game.Properties.Resources.obstacle_2;
+            this.pictureBox3.Location = new System.Drawing.Point(435, 364);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 33);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstacle";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox4.Location = new System.Drawing.Point(-2, 406);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(812, 44);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainGameTimer);
+            // 
+            // textScore
+            // 
+            this.textScore.AutoSize = true;
+            this.textScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textScore.Location = new System.Drawing.Point(13, 13);
+            this.textScore.Name = "textScore";
+            this.textScore.Size = new System.Drawing.Size(69, 25);
+            this.textScore.TabIndex = 4;
+            this.textScore.Text = "Score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(791, 453);
+            this.Controls.Add(this.trex);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.textScore);
+            this.Controls.Add(this.pictureBox4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T-Rex-Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox trex;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label textScore;
+    }
+}
+
---
+++ T-Rex Game/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_Rex_Game
+{
+    public partial class Form1 : Form
+    {
+        int position;
+        int ObstacleSpeed = 10;
+        Random ran = new Random();
+        int force = 12;
+        bool Jumping = false;
+        bool IsGameOver = false;
+        int score = 0;
+        int JumpSpeed = 12;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //GameReset();
+        }
+
+        private void MainGameTimer(object sender, EventArgs e)
+        {
+            trex.Top += JumpSpeed;
+           textScore.Text = "Score : " + score;
+           
+            if (Jumping==true && force<0)
+            {
+                Jumping = false;
+            }
+            if (Jumping == true )
+            {
+                force -= 1;
+                JumpSpeed =- 12;
+            }
+            else
+            {
+                JumpSpeed = 12;
+            }
+            if (trex.Top > 280 && Jumping == false )
+            {
+                force = 12;
+                JumpSpeed = 0;
+                trex.Top = 287;
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left -= ObstacleSpeed;
+                    if (x.Left < -100)
+                    {
+                        position = this.ClientSize.Width +ran.Next(500,800)+ (x.Width * 25);
+                        x.Left = position;
+                        score++;
+                        if (score >= 10)
+                        {
+                            ObstacleSpeed = 15;
+                        }
+
+                    }
+
+                    if (trex.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gameTimer.Stop();
+                        trex.Image = Properties.Resources.dead;
+                        textScore.Text += "   Press R for Restart the game!";
+                        IsGameOver = true;
+                    }
+                }
+            }
+        }
+        private void GameReset()
+        {
+            force = 12;
+            Jumping = false;
+            JumpSpeed=0;
+            score = 0;
+            ObstacleSpeed = 10;
+            trex.Image = Properties.Resources.running;
+            IsGameOver = false;
+            trex.Top = 287;
+            textScore.Text = "Score :" + score;
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    position = this.ClientSize.Width+ran.Next(500, 800)+(x.Width*25);
+                    x.Left = position;
+                }
+            }
+            gameTimer.Start();
+            
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && Jumping == false)
+            {
+                Jumping = true;
+            }
+           
+        }
+     
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (Jumping == true)
+            {
+                Jumping = false;
+            }
+            if (e.KeyCode == Keys.R && IsGameOver == true)
+            {
+                GameReset();
+            }
+        }
+
+      
+    }
+}
